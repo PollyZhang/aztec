d0aca789834118107a376a43d03da0b32a28b553
Zhang xiaolin <3965488@qq.com>
2013-11-01 22:14:46 +0800

    aaa

--- tinyshop/src/com/sosee/web/controller/IndexController.java
+++ tinyshop/src/com/sosee/web/controller/IndexController.java
@@ -18,25 +18,51 @@ public class IndexController extends WebController{
 
 
 	public void index(){
-		//this.createToken("contentsToken",1800);
-		render("/WEB-INF/web/index.html");
+		try {
+			List<Record> renmeixinwenList = Db.find("SELECT c.title,c.id,c.imageFile FROM tinyshop.t_contents as c"
+					+ " where c.contentCategoryId = '509ffc01-2b79-4bfe-bc5a-675ebfe05208'");
+			this.setAttr("renmeixinwenList", renmeixinwenList);
+			List<Record> taokefengcaiList = Db.find("SELECT c.title,c.id,c.imageFile FROM tinyshop.t_contents as c"
+					+ " where c.contentCategoryId = '7c24be68-0929-4b60-9f84-fab4ef6bc0c0'");
+			this.setAttr("taokefengcaiList", taokefengcaiList);
+			List<Record> qiyefengcaiList = Db.find("SELECT c.title,c.id,c.imageFile FROM tinyshop.t_contents as c"
+					+ " where c.contentCategoryId = '39b1ccc3-e7b8-4042-8fda-148538fb7b57'");
+			this.setAttr("qiyefengcaiList", qiyefengcaiList);
+			List<Record> imageNewsList = Db.find("SELECT c.title,c.id,c.imageFile FROM tinyshop.t_contents as c"
+					+ " where c.contentCategoryId = '2e51ba23-4757-46f3-bd95-aac08121b18c'");
+			this.setAttr("imageNewsList", imageNewsList);
+			render("/WEB-INF/web/index.html");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void contentListView()
 	{
-		String contentCategoryId = getPara("contentCategoryId");
-		List<Record> contentList = Db.find("select c.title,subTitle,"
-				+ "c.author,c.contents,c.newsDate  from t_contents as c");
-		this.setAttr("contentList",contentList);
-		render("/WEB-INF/web/listContent.html");
+		try {
+			String contentCategoryId = getPara("contentCategoryId");
+			List<Record> contentList = Db.find("select c.title,subTitle,"
+					+ "c.author,c.contents,c.newsDate  from t_contents as c");
+			this.setAttr("contentList",contentList);
+			render("/WEB-INF/web/listContent.html");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void contentView()
 	{
-		String contentId = getPara("contentId");
-		Contents content = getModel(Contents.class).findById(contentId);
-		this.setAttr("content", content);
-		render("/WEB-INF/web/content.html");
+		try {
+			String contentId = getPara("contentId");
+			Contents content = getModel(Contents.class).findById(contentId);
+			this.setAttr("content", content);
+			render("/WEB-INF/web/content.html");	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
